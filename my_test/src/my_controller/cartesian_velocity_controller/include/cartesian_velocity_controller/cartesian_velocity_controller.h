d88c9def367fe32ee6830f5c8e50c6208bb4da40
mrzhuzhe <466648706@qq.com>
2022-12-17 18:35:32 +0800

    need known why velocity control

--- my_test/src/my_controller/cartesian_velocity_controller/include/cartesian_velocity_controller/cartesian_velocity_controller.h
+++ my_test/src/my_controller/cartesian_velocity_controller/include/cartesian_velocity_controller/cartesian_velocity_controller.h
@@ -3,12 +3,22 @@
 
 
 #include <ros/node_handle.h>
-
 #include <controller_interface/controller.h>
 #include <hardware_interface/joint_command_interface.h>
 
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose.h>
+#include <cartesian_state_msgs/PoseTwist.h>
+#include <kdl/chainiksolvervel_pinv.hpp>
+#include <kdl/chainiksolvervel_pinv_givens.hpp>
+#include <kdl/chainiksolverpos_lma.hpp>
+#include <kdl/chainfksolvervel_recursive.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <realtime_tools/realtime_publisher.h>
+#include "kdl_base.h"
+
 namespace cartesian_velocity_controller {
-    class Cartesian_Velocity_Controller: public controller_interface::Controller<hardware_interface::PositionJointInterface>{
+    class Cartesian_Velocity_Controller: public kdl_base::KDL_Base {
         public:
             Cartesian_Velocity_Controller() {}
             ~Cartesian_Velocity_Controller() {}
@@ -39,19 +49,35 @@ namespace cartesian_velocity_controller {
             * \brief Issues commands to the joint. Called at regular intervals
             */
             void update(const ros::Time& time, const ros::Duration& period);
+
+            /*!
+            * \brief Subscriber's callback function
+            */
+            void command_cart_vel(const geometry_msgs::TwistConstPtr &msg);
         private:
             /** \brief Write current commands to the hardware interface
             */
             void writeVelocityCommands(const ros::Duration& period);
-            void writePositionCommands(const ros::Duration& period);
                     
         protected:
-            /*
+            ros::Subscriber                 sub_command_; // Interface to external commands
+
+            ros::Time                       last_publish_time_;
+            double                          publish_rate_;
+
             KDL::JntArray                   Jnt_Vel_Cmd_;      // Desired joint velocity
             KDL::Twist                      End_Vel_Cmd_;      // Desired end-effector velocity
-            KDL::JntArray                   Jnt_Pos_Cmd_;      // Desired joint position
-            KDL::Frame                      End_Pos_Cmd_;      // Desired end-effector position
-            */
+            KDL::FrameVel                   End_Vel_;
+            KDL::Frame                      End_Pos_;
+            cartesian_state_msgs::PoseTwist msg_state_;
+
+
+            boost::shared_ptr<KDL::ChainFkSolverVel> fk_vel_solver_;
+            boost::shared_ptr<KDL::ChainFkSolverPos> fk_pos_solver_;
+            boost::shared_ptr<KDL::ChainIkSolverVel> ik_vel_solver_;
+
+            boost::shared_ptr<realtime_tools::RealtimePublisher<
+                cartesian_state_msgs::PoseTwist> > realtime_pub_;
 
     };
 }
--- my_test/src/my_controller/cartesian_velocity_controller/src/cartesian_velocity_controller.cpp
+++ my_test/src/my_controller/cartesian_velocity_controller/src/cartesian_velocity_controller.cpp
@@ -1,9 +1,10 @@
 
 #include <cartesian_velocity_controller/cartesian_velocity_controller.h>
-
-#include <hardware_interface/hardware_interface.h>
 #include <pluginlib/class_list_macros.h>
-#include <ros/ros.h>
+#include "kdl_conversions/kdl_msg.h"
+
+#include "cartesian_velocity_controller/kdl_base.h"
+
 
 namespace cartesian_velocity_controller {
     /** \brief Initialize the kinematic chain for kinematics-based computation.
@@ -21,6 +22,39 @@ namespace cartesian_velocity_controller {
         } else {
             ROS_INFO_STREAM("test_value: " << test_value);
         }
+
+        ros::NodeHandle &n = node_handle;
+        // KDL
+        kdl_base::KDL_Base::init(robot, n);
+
+        ik_vel_solver_.reset(new KDL::ChainIkSolverVel_pinv_givens(this->kdl_chain_));
+        fk_vel_solver_.reset(new KDL::ChainFkSolverVel_recursive(this->kdl_chain_));
+        fk_pos_solver_.reset(new KDL::ChainFkSolverPos_recursive(this->kdl_chain_));
+
+        // get publishing period
+        if (!n.getParam("publish_rate", publish_rate_)){
+            ROS_ERROR("Parameter 'publish_rate' not set");
+            return false;
+        }
+        realtime_pub_.reset(new realtime_tools::RealtimePublisher
+            <cartesian_state_msgs::PoseTwist>(n, "ee_state", 4));
+
+
+        // Topics
+        sub_command_ = n.subscribe("command_cart_vel", 5,
+            &Cartesian_Velocity_Controller::command_cart_vel, this,
+            ros::TransportHints().reliable().tcpNoDelay());
+
+        // Variable init
+        this->joint_state_.resize(this->kdl_chain_.getNrOfJoints());
+        this->joint_effort_.resize(this->kdl_chain_.getNrOfJoints());
+        Jnt_Vel_Cmd_.resize(this->kdl_chain_.getNrOfJoints());
+        End_Vel_Cmd_ = KDL::Twist::Zero();
+        End_Pos_.p.Zero();
+        End_Pos_.M.Identity();
+        End_Vel_.p.Zero();
+        End_Vel_.M.Identity();
+
         return true;
     }
 
@@ -33,6 +67,14 @@ namespace cartesian_velocity_controller {
 
         
         ROS_INFO("Cartesian_Velocity_Controller starting");
+        for(std::size_t i=0; i < this->joint_handles_.size(); i++) {
+            Jnt_Vel_Cmd_(i) = 0.0;
+            this->joint_state_.q(i)     = 0.0;
+            this->joint_state_.qdot(i)  = 0.0;
+            this->joint_effort_(i)    = 0.0;
+        }
+        End_Vel_Cmd_ = KDL::Twist::Zero();
+        last_publish_time_ = time;
     }
 
     /*!
@@ -40,8 +82,53 @@ namespace cartesian_velocity_controller {
     */
     void Cartesian_Velocity_Controller::update(const ros::Time& time, const ros::Duration& period) {
         
-        
+        // Get joint positions
+        for(std::size_t i=0; i < this->joint_handles_.size(); i++)
+        {
+            // if(fabs(this->joint_handles_[i].getPosition()-(this->joint_state_.q(i) + Jnt_Vel_Cmd_(i)*period.toSec()))<0.0000001 || Jnt_Vel_Cmd_(i)!=0)
+            // {
+            this->joint_state_.q(i)         = this->joint_handles_[i].getPosition();
+            this->joint_state_.qdot(i)      = this->joint_handles_[i].getVelocity();
+            this->joint_effort_(i)        = this->joint_handles_[i].getEffort();
+            // }
+        }
+        // Compute inverse kinematics velocity solver
+        ik_vel_solver_->CartToJnt(this->joint_state_.q, End_Vel_Cmd_, Jnt_Vel_Cmd_);
+        writeVelocityCommands(period);
+
+        // Forward kinematics
+        fk_vel_solver_->JntToCart(this->joint_state_, End_Vel_);
+        fk_pos_solver_->JntToCart(this->joint_state_.q, End_Pos_);
+
+        // Limit rate of publishing
+        if (publish_rate_ > 0.0 && last_publish_time_
+            + ros::Duration(1.0/publish_rate_) < time) {
+
+            // try to publish
+            if (realtime_pub_->trylock()) {
+            last_publish_time_ = last_publish_time_
+                                + ros::Duration(1.0/publish_rate_);
+            // populate message
+            realtime_pub_->msg_.header.stamp = time;
+            tf::poseKDLToMsg(End_Pos_, realtime_pub_->msg_.pose);
+            tf::twistKDLToMsg(End_Vel_.GetTwist(), realtime_pub_->msg_.twist);
+
+            realtime_pub_->unlockAndPublish();
+            }
+        }
         //ROS_INFO_STREAM("LOOPRATE");
+    }
+
+    /*!
+    * \brief Subscriber's callback: copies twist commands
+    */
+    void Cartesian_Velocity_Controller::command_cart_vel(const geometry_msgs::TwistConstPtr &msg) {
+        End_Vel_Cmd_.vel(0) = msg->linear.x;
+        End_Vel_Cmd_.vel(1) = msg->linear.y;
+        End_Vel_Cmd_.vel(2) = msg->linear.z;
+        End_Vel_Cmd_.rot(0) = msg->angular.x;
+        End_Vel_Cmd_.rot(1) = msg->angular.y;
+        End_Vel_Cmd_.rot(2) = msg->angular.z;
     }
 
     /********************************************/
@@ -49,17 +136,15 @@ namespace cartesian_velocity_controller {
     /********************************************/
 
     /** \brief write the desired velocity command in the hardware interface input
-    * for a VelocityJointInterface
+    * for a PositionJointInterface
     * \param period The duration of an update cycle
     */
     void Cartesian_Velocity_Controller::writeVelocityCommands(
                                         const ros::Duration& period) {
-
-    }
-
-    void Cartesian_Velocity_Controller::writePositionCommands(
-                                        const ros::Duration& period)  {
-        
+        for(std::size_t i=0; i < this->joint_handles_.size(); i++) {
+        this->joint_handles_[i].setCommand(this->joint_state_.q(i)
+                                        + Jnt_Vel_Cmd_(i)*period.toSec());
+        }
     }
 
 }   
